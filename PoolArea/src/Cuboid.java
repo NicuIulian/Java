831d3c01ef0b45f1ba970f7301a68353b11c0378
Țîu Nicu-Iulian <102763293+NicuIulian@users.noreply.github.com>
2023-05-15 14:20:08 +0300

    Add files via upload

---
+++ PoolArea/src/Cuboid.java
@@ -0,0 +1,23 @@
+public class Cuboid extends Rectangle{
+    private double height;
+
+    public double getHeight() {
+        return height;
+    }
+
+    public Cuboid(double width, double length, double height) {
+        super(width, length);
+      if(height <0){
+          this.height =0;
+      }else {
+          this.height =height;
+        }
+    }
+
+    public Cuboid(double height) {
+        this.height = height;
+    }
+    public double getVolume(){
+        return  getArea() * height;
+    }
+}
